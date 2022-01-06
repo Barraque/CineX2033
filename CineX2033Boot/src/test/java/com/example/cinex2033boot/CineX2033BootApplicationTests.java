458b5c0f40bc5a03f9ee47ca569fee4d0c52826d
louis <louis.barraque@gmail.com>
2022-01-06 14:17:58 +0100

    init project

---
+++ CineX2033Boot/src/test/java/com/example/cinex2033boot/CineX2033BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinex2033boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineX2033BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
