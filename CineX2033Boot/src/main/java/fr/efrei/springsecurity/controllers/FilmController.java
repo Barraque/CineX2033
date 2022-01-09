0aefc1a4585b9561df5ed0f0bfe28ec8655dc677
louis <louis.barraque@gmail.com>
2022-01-09 18:59:50 +0100

    fix bug

--- CineX2033Boot/src/main/java/fr/efrei/springsecurity/controllers/FilmController.java
+++ CineX2033Boot/src/main/java/fr/efrei/springsecurity/controllers/FilmController.java
@@ -1,6 +1,5 @@
 package fr.efrei.springsecurity.controllers;
 
-import fr.efrei.springsecurity.models.Cinema;
 import fr.efrei.springsecurity.models.Film;
 import fr.efrei.springsecurity.services.auth.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
