e1c914158708f5510567f7b67e59eb56028059ab
Loulou <louis.barraque@protonmail.com>
2022-01-20 13:06:31 +0100

    set good security conf

--- CineX2033Boot/src/main/java/fr/efrei/springsecurity/configs/auth/BasicAuthSecurityConfig.java
+++ CineX2033Boot/src/main/java/fr/efrei/springsecurity/configs/auth/BasicAuthSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,8 +34,8 @@ public class BasicAuthSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth").hasAnyRole(Role.USER).and().httpBasic()
-                .and().authorizeRequests().anyRequest().permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().hasAnyRole(Role.USER, Role.ADMIN).and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
